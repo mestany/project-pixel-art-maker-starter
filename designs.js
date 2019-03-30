d288371a0e2f50a336ac0ed550a97d9f60a18491
mestany <48977149+mestany@users.noreply.github.com>
2019-03-29 19:05:23 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,5 +1,51 @@
 // Select color input
 // Select size input
+var height, width, color;
+
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width =$('#inputWidth').val();
+    makeGrid(height, width);
+    //console.log('Height:' + height + 'and width' + with);
+})
+
+
+function makeGrid(x,y) {
+  $('tr').remove();
+
+ // Your code goes here!
+ for(var i=1; i <= x; i++){
+   $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+   for (var j=1; j <=y; j++) {
+       $('#table' + i).append('<td></td>');
+   }
+ }
+
+ //add color to cell
+ $('td').click(function addColor() {
+    color= $('#colorPicker').val();
+
+    if($(this).attr('style')) {
+      $(this).removeAttr('style')
+    } else{
+      $(this).attr('style', 'background-color:' + color);
+    }
+ })
+}
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
+ss// Select color input
+// Select size input
 
 // When size is submitted by the user, call makeGrid()
 
